14446c24e03fd8b57a215ccd07970ab297f98a72
hdudek <haniadudek05@gmail.com>
2025-05-08 21:49:18 +0200

    Revert "oddzielne wątki dla kulek"
    This reverts commit 3086db4b559f7ceb40e07abbe978b15e1eec467f.

--- ConcurrentProgramming/ReactiveInteractiveUserInterface/Data/DataImplementation.cs
+++ ConcurrentProgramming/ReactiveInteractiveUserInterface/Data/DataImplementation.cs
@@ -9,14 +9,24 @@
 //_____________________________________________________________________________________________________________________________________
 
 using System;
-using System.Collections.Generic;
-using System.Timers;
 using System.Diagnostics;
 
 namespace TP.ConcurrentProgramming.Data
 {
   internal class DataImplementation : DataAbstractAPI
   {
+    #region ctor
+
+    public DataImplementation()
+    {
+      MoveTimer = new Timer(Move, null, TimeSpan.Zero, TimeSpan.FromMilliseconds(1000/60));
+    }
+
+    #endregion ctor
+
+    private const double tableWidth = 400.0;
+    private const double tableHeight = 400.0;
+    private const double ballRadius = 10.0;
 
     #region DataAbstractAPI
 
@@ -26,36 +36,23 @@ namespace TP.ConcurrentProgramming.Data
         throw new ObjectDisposedException(nameof(DataImplementation));
       if (upperLayerHandler == null)
         throw new ArgumentNullException(nameof(upperLayerHandler));
-
-      foreach (System.Timers.Timer t in _timers)
+      lock (BallsList)
       {
-          t.Stop();
-          t.Dispose();
-      }
-      _timers.Clear();
-      _balls.Clear();
-
-      Random random = new Random();
-      for (int i = 0; i < numberOfBalls; i++)
-      {
-        Vector startingPosition = new(random.Next(100, (int)tableWidth - 100), random.Next(100, (int)tableHeight - 100));
-        double angle = 2 * Math.PI * random.NextDouble();
-        double speed = 2.0;
-        double vx = speed * Math.Cos(angle);
-        double vy = speed * Math.Sin(angle);
-        Vector initialVelocity = new Vector(vx, vy);
-
-        Ball newBall = new(startingPosition, initialVelocity);
-        _balls.Add(newBall);
-        upperLayerHandler(startingPosition, newBall);
-
-        System.Timers.Timer timer = new(1000 / 60)
+        BallsList.Clear();
+        Random random = new Random();
+        for (int i = 0; i < numberOfBalls; i++)
         {
-            AutoReset = true
-        };
-        timer.Elapsed += (s, e) => Move(newBall);
-        timer.Start();
-        _timers.Add(timer);
+            Vector startingPosition = new(random.Next(100, (int)tableWidth - 100), random.Next(100, (int)tableHeight - 100));
+            double angle = 2 * Math.PI * random.NextDouble();
+            double speed = 2.0;
+            double vx = speed * Math.Cos(angle);
+            double vy = speed * Math.Sin(angle);
+            Vector initialVelocity = new Vector(vx, vy);
+
+            Ball newBall = new(startingPosition, initialVelocity);
+            upperLayerHandler(startingPosition, newBall);
+            BallsList.Add(newBall);
+        }
       }
     }
 
@@ -69,13 +66,8 @@ namespace TP.ConcurrentProgramming.Data
       {
         if (disposing)
         {
-          foreach (System.Timers.Timer t in _timers)
-          {
-            t.Stop();
-            t.Dispose();
-          }
-          _timers.Clear();
-          _balls.Clear();
+          MoveTimer.Dispose();
+          BallsList.Clear();
         }
         Disposed = true;
       }
@@ -97,65 +89,63 @@ namespace TP.ConcurrentProgramming.Data
     //private bool disposedValue;
     private bool Disposed = false;
 
+    private readonly Timer MoveTimer;
     private Random RandomGenerator = new();
+    private List<Ball> BallsList = [];
 
-    private const double tableWidth = 400.0;
-    private const double tableHeight = 400.0;
-    private const double ballRadius = 10.0;
-
-    private readonly List<Ball> _balls = new();
-    private readonly List<System.Timers.Timer> _timers = new();
-    private readonly object _lock = new();
-
-    private void Move(Ball ball)
+    private void Move(object? x)
     {
-      lock (_lock) 
-      {
-        ball.Move(new Vector(ball.Velocity.x, ball.Velocity.y));
-        Vector position = ball.Position;
-        if ((position.x - ballRadius <= 0 && ball.Velocity.x < 0) || (position.x + ballRadius >= tableWidth && ball.Velocity.x > 0))
+      lock (BallsList) {
+        foreach (Ball ball in BallsList)
         {
-            ball.Velocity = new Vector(-ball.Velocity.x, ball.Velocity.y);
-        }
-        if ((position.y - ballRadius <= 0 && ball.Velocity.y < 0) || (position.y + ballRadius >= tableHeight && ball.Velocity.y > 0))
-        {
-            ball.Velocity = new Vector(ball.Velocity.x, -ball.Velocity.y);
-        }
-
-        foreach (Ball otherBall in _balls)
-        {
-            if (otherBall == ball) continue;
-
-            Vector delta = new Vector(
-                otherBall.Position.x - ball.Position.x,
-                otherBall.Position.y - ball.Position.y);
-
-            double distance = Math.Sqrt(delta.x * delta.x + delta.y * delta.y);
-
-            if (distance < 2 * ballRadius) // kolizja
+            ball.Move(new Vector(ball.Velocity.x, ball.Velocity.y));
+            Vector position = ball.Position;
+            if ((position.x - ballRadius <= 0 && ball.Velocity.x < 0) || (position.x + ballRadius >= tableWidth && ball.Velocity.x > 0))
             {
-                // wyznaczenie wektora normalnego (kierunek kolizji)
-                Vector normal = new Vector(delta.x / distance, delta.y / distance);
-                Vector tangent = new Vector(-normal.y, normal.x); // wektor styczny
-                double v1n = ball.Velocity.x * normal.x + ball.Velocity.y * normal.y; // prędkość w kierunku normalnym
-                double v2n = otherBall.Velocity.x * normal.x + otherBall.Velocity.y * normal.y;
-                double v1t = ball.Velocity.x * tangent.x + ball.Velocity.y * tangent.y; // prędkość w kierunku stycznym
-                double v2t = otherBall.Velocity.x * tangent.x + otherBall.Velocity.y * tangent.y;
-                // nowa prędkość w kierunku normalnym
-                // składowe styczne pozostają niezmienione a normalne się wymieniają
-                double newV1x = (v2n * normal.x) + (v1t * tangent.x);
-                double newV1y = (v2n * normal.y) + (v1t * tangent.y);
-                double newV2x = (v1n * normal.x) + (v2t * tangent.x);
-                double newV2y = (v1n * normal.y) + (v2t * tangent.y);
-                ball.Velocity = new Vector(newV1x, newV1y);
-                otherBall.Velocity = new Vector(newV2x, newV2y);
-
-                // jeśli kulki są " w sobie" to przesuwamy je do pozycji styku
-                double overlap = 2 * ballRadius - distance;
-                ball.Move(new Vector(-normal.x * overlap / 2, -normal.y * overlap / 2));
-                otherBall.Move(new Vector(normal.x * overlap / 2, normal.y * overlap / 2));
+                ball.Velocity = new Vector(-ball.Velocity.x, ball.Velocity.y);
             }
-         
+            if ((position.y - ballRadius <= 0 && ball.Velocity.y < 0) || (position.y + ballRadius >= tableHeight && ball.Velocity.y > 0))
+            {
+                ball.Velocity = new Vector(ball.Velocity.x, -ball.Velocity.y);
+            }
+        }
+        for (int i = 0; i < BallsList.Count; i++)
+        {
+            for (int j = i + 1; j < BallsList.Count; j++)
+            {
+                Ball ball1 = BallsList[i];
+                Ball ball2 = BallsList[j];
+
+                Vector delta = new Vector(
+                    ball2.Position.x - ball1.Position.x,
+                    ball2.Position.y - ball1.Position.y);
+
+                double distance = Math.Sqrt(delta.x * delta.x + delta.y * delta.y);
+
+                if (distance < 2 * ballRadius) // kolizja
+                {
+                    // wyznaczenie wektora normalnego (kierunek kolizji)
+                    Vector normal = new Vector(delta.x / distance, delta.y / distance);
+                    Vector tangent = new Vector(-normal.y, normal.x); // wektor styczny
+                    double v1n = ball1.Velocity.x * normal.x + ball1.Velocity.y * normal.y; // prędkość w kierunku normalnym
+                    double v2n = ball2.Velocity.x * normal.x + ball2.Velocity.y * normal.y;
+                    double v1t = ball1.Velocity.x * tangent.x + ball1.Velocity.y * tangent.y; // prędkość w kierunku stycznym
+                    double v2t = ball2.Velocity.x * tangent.x + ball2.Velocity.y * tangent.y;
+                    // nowa prędkość w kierunku normalnym
+                    // składowe styczne pozostają niezmienione a normalne się wymieniają
+                    double newV1x = (v2n * normal.x) + (v1t * tangent.x);
+                    double newV1y = (v2n * normal.y) + (v1t * tangent.y);
+                    double newV2x = (v1n * normal.x) + (v2t * tangent.x);
+                    double newV2y = (v1n * normal.y) + (v2t * tangent.y);
+                    ball1.Velocity = new Vector(newV1x, newV1y);
+                    ball2.Velocity = new Vector(newV2x, newV2y);
+
+                    // jeśli kulki są " w sobie" to przesuwamy je do pozycji styku
+                    double overlap = 2 * ballRadius - distance;
+                    ball1.Move(new Vector(-normal.x * overlap / 2, -normal.y * overlap / 2));
+                    ball2.Move(new Vector(normal.x * overlap / 2, normal.y * overlap / 2));
+                }
+            }
         }
       }
     }
@@ -166,13 +156,13 @@ namespace TP.ConcurrentProgramming.Data
     [Conditional("DEBUG")]
     internal void CheckBallsList(Action<IEnumerable<IBall>> returnBallsList)
     {
-      returnBallsList(_balls);
+      returnBallsList(BallsList);
     }
 
     [Conditional("DEBUG")]
     internal void CheckNumberOfBalls(Action<int> returnNumberOfBalls)
     {
-      returnNumberOfBalls(_balls.Count);
+      returnNumberOfBalls(BallsList.Count);
     }
 
     [Conditional("DEBUG")]
